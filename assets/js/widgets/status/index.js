cd278bea50d9233524ab35fd96a167acae14660f
James Kyburz <james.kyburz@gmail.com>
2014-12-06 18:37:42 +0100

    Status widget

---
+++ assets/js/widgets/status/index.js
@@ -0,0 +1,29 @@
+var domify       = require('domify');
+var fs           = require('fs');
+var insertCss    = require('insert-css');
+
+module.exports = Status;
+
+function Status() {
+  if (!(this instanceof Status)) return new Status();
+  insertCss(fs.readFileSync(__dirname + '/style.css', 'utf-8'));
+  this.html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+}
+
+Status.prototype.appendTo = function appendTo(el) {
+  if (this.el) return this;
+  this.el = el.appendChild(domify(this.html));
+};
+
+Status.prototype.update = function update(err, successText) {
+  var el = this.el;
+  reset();
+  el.classList.add(err ? 'error' : 'success');
+  el.textContent = !err ? successText : err;
+  setTimeout(reset, 3000);
+
+  function reset() {
+    el.classList.remove('success');
+    el.classList.remove('error');
+  }
+};
