b996cb0b5ef390ab84889944a494dfcbc55007e9
James Kyburz <james.kyburz@gmail.com>
2015-02-18 20:07:07 +0100

    security headers for sessioncam

--- assets/js/app.js
+++ assets/js/app.js
@@ -142,6 +142,6 @@ function checkApiResponse(response) {
 // +sessioncam
 var scRec=document.createElement('SCRIPT');
 scRec.type='text/javascript';
-scRec.src="//d2oh4tlt9mrke9.cloudfront.net/Record/js/sessioncam.recorder.js";
+scRec.src="https://d2oh4tlt9mrke9.cloudfront.net/Record/js/sessioncam.recorder.js";
 document.getElementsByTagName('head')[0].appendChild(scRec);
 // -sessioncam
