21ec8e734a68758d43a767610b5aa307f706f6d0
James Kyburz <james.kyburz@gmail.com>
2015-02-19 19:31:19 +0100

    Use fontello-mount istead of fontello-cli

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var http       = require('http');
 var routes     = require('./lib/routes');
 var config     = require('./config');
+var fontConfig = require('./fontello/config.json');
+var mountFonts = require('fontello-mount');
 
 var port = process.env.PORT || config.port;
 
-routes.port = port;
+mountFonts(fontConfig, start);
 
-var server = http.createServer(routes).
-  listen(port, running)
-;
-
-function running() {
-  console.log('listening on http://localhost:' + port);
+function start(err, fontello) {
+  if (err) return console.error(err);
+  routes.port = port;
+  routes.fontello = fontello;
+  var server = http.createServer(routes);
+  server.listen(port, console.log.bind(console, 'running on http://localhost:%s', port));
 }
